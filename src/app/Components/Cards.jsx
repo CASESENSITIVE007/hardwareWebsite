4bbe3ef36c606d6ba82fe4c300cd2f57e27fce01
Mohammad Saad <mohammadsaad3498@gmail.com>
2025-07-29 03:49:35 +0530

    cards

---
+++ src/app/Components/Cards.jsx
@@ -0,0 +1,26 @@
+import Card from "./Card"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+function Cards() {
+  return (
+    <div className="text-center m-10 ">
+        <h1 className="md:text-6xl text-5xl font-red m-10  text-center font-bold " >Discover 50+ product choices</h1>
+    <div className='md:flex justify-center gap-10  grid text-center  ' >
+      
+        <div className="gap-10 flex flex-col" >
+            <Card Name="Ball Bearing"/>
+            <Card Name="Lubricant"/>
+            </div>
+        <div  className="gap-10 flex flex-col"> 
+            <Card/>
+            <Card/>
+        </div>
+    
+  
+    </div>
+   <Link href='/Products' > <Button  className="m-10 cursor-pointer" >ALL Products</Button></Link>
+    </div>
+  )
+}
+
+export default Cards
--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,14 @@ import Contactus from "./Components/Contactus"
 import Navbar from "./Components/Navbar";
 import KeyIndustries from "./Components/KeyIndustries";
 import About from './Components/About';
+import Cards from "./Components/Cards";
 
 export default function Home() {
   return (
    <div>
     <Navbar/>
     <Hero/>
+    <Cards/>
     <Contactus/>
     <KeyIndustries/>
     <About/>
