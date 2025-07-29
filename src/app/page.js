a5e9b7ea279c5e7ab55f4bfaaf7455a4f7a5b981
Mohammad Saad <mohammadsaad3498@gmail.com>
2025-07-29 16:17:47 +0530

    add features

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
 
 import { Button } from "@/components/ui/button"
+
 import Hero from "./Components/Hero"
 import Contactus from "./Components/Contactus"
+import Features from "./Components/Features"
 import Navbar from "./Components/Navbar";
 import KeyIndustries from "./Components/KeyIndustries";
 import About from './Components/About';
@@ -12,6 +14,7 @@ export default function Home() {
    <div>
     <Navbar/>
     <Hero/>
+    <Features/>
     <Cards/>
     <Contactus/>
     <KeyIndustries/>
