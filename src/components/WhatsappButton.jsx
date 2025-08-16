7be2adb27fda0cb1d2c7cc9b29741e3162f74137
Mohammad Saad <mohammadsaad3498@gmail.com>
2025-08-16 13:25:14 +0530

    footer linksx

--- src/components/WhatsappButton.jsx
+++ src/components/WhatsappButton.jsx
@@ -4,12 +4,12 @@ import  {FaWhatsapp}  from "react-icons/fa";
 const WhatsappButton = () => {
   return (
     <a
-      href="https://wa.me/919876543210" // Replace with your WhatsApp number (with country code, no + or spaces)
+      href="https://wa.me/917827505517" // Replace with your WhatsApp number (with country code, no + or spaces)
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-5 right-5 bg-green-500 text-white p-3 rounded-full shadow-lg hover:bg-green-600 transition-all duration-300 z-50"
     >
-      <FaWhatsapp size={44} />
+      <FaWhatsapp size={70} />
     </a>
   );
 };
